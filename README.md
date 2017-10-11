77ae10ce46516be09a1f3ff3abfaa05bea5fea79
Daichi Fukuoka <daichi.fukuoka@hpe.com>
2017-10-11 13:23:57 +0900

    add dgemm.py

--- README.md
+++ README.md
@@ -12,8 +12,8 @@ iter: 20 x: 0.250000000000037 diff: 2.964295475749168e-14
 converged, iter: 25
 minimum value at x = 0.25
 ~~~
- * gd_2d.py: gradient descent with two dimensional function. numpy is needed.
- ~~~
+ * gd_2d.py: gradient descent with two dimensional function.
+~~~
  $ ./gd_2d.py 0.4
 alpha: 0.4 offset: [ 0.25  0.5 ]
 iter: 0 x: [ 1.  2.] diff: [ 0.0360328  0.0720656]
@@ -22,4 +22,12 @@ iter: 20 x: [ 0.25  0.5 ] diff: [  1.41628875e-10   2.83257751e-10]
 iter: 30 x: [ 0.25  0.5 ] diff: [ 0.  0.]
 converged, iter: 30
 minimum value at x = [ 0.25  0.5 ]
- ~~~
+~~~
+ 
+ * dgemm.py: matrix-matrix multiplication by python.
+~~~
+$ ./dgemm.py
+dtype: float64
+matrix size: 128 x 128
+dgemm time: 0.0002474784851074219 trace: 4110.47098433
+~~~
---
+++ dgemm.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import time as t
+
+# matrix product c = a*b
+
+def dgemm(a, b, c):
+    np.dot(a, b, out=c)
+
+"""
+# C like manner by for loop, too slow for python, though the result is correct
+def dgemm(a, b, c):
+    size = len(a[0][:])
+    for i in range(size):
+        for j in range(size):
+            for k in range(size):
+                c[i][j] += a[i][k]*b[k][j]
+"""
+ 
+def trace(c, size):
+    tr = 0.0
+    for i in range(size):
+        tr = tr + c[i][i]
+    
+    return tr
+
+def main():
+    size = 128
+    seed = 7777
+    np.random.seed(seed)
+    a = np.random.rand(size, size).astype(np.float64)
+    b = np.random.rand(size, size).astype(np.float64)
+    c = np.zeros((size, size), dtype=np.float64)
+
+    print("dtype:", a.dtype)
+    
+    t0 = t.time()
+    dgemm(a, b, c)
+    time = t.time() - t0
+    print("matrix size:", size, "x", size)
+    print("dgemm time:", time, "trace:", trace(c, size))
+    
+if __name__ == "__main__":
+    main()
